be1fa461771e244652c085a7bb93e8dd8edcf56d
Jura <barmalej@live.ru>
2024-01-08 23:30:56 +0700

    Добавьте файлы проекта.

---
+++ OTUS_L21_HW/Fibonacci.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+
+namespace OTUS_L21_HW
+{
+    internal class Fibonacci
+    {
+        public static int Recursion(int n)
+        {
+            return n > 1 ? Recursion(n - 1) + Recursion(n - 2) : n;
+        }
+
+        public static int Iteration(int n)
+        {
+            if (n == 0) 
+                return 0;
+            if (n == 1) 
+                return 1;
+
+            int _num0 = 0;
+            int _num1 = 1;
+            int _result = 0;
+            
+            for (int i = 2; i <= n; i++)
+            {
+                _result = _num0 + _num1;
+                _num0 = _num1;
+                _num1 = _result;
+            }
+            
+            return _result;
+        }
+        public static (int, TimeSpan) RecursionGetTime(int number)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            var result = Recursion(number);
+            stopWatch.Stop();
+
+            return (result, stopWatch.Elapsed);
+        }
+
+        public static (int, TimeSpan) IterationGetTime(int number)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            var result = Iteration(number);
+            stopWatch.Stop();
+
+            return (result, stopWatch.Elapsed);
+        }
+    }
+}
---
+++ OTUS_L21_HW/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace OTUS_L21_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Рекурсия");
+            //Console.WriteLine(Fibonacci.Recursion(5));
+            //Console.WriteLine(Fibonacci.Recursion(10));
+            //Console.WriteLine(Fibonacci.Recursion(20));
+
+            //Console.WriteLine("Цикл");
+            //Console.WriteLine(Fibonacci.Iteration(5));
+            //Console.WriteLine(Fibonacci.Iteration(10));
+            //Console.WriteLine(Fibonacci.Iteration(20));
+
+
+            Console.WriteLine("Рекурсия");
+            PrintResult(Fibonacci.RecursionGetTime(5));
+            PrintResult(Fibonacci.RecursionGetTime(10));
+            PrintResult(Fibonacci.RecursionGetTime(20));
+
+            Console.WriteLine("Цикл");
+            PrintResult(Fibonacci.IterationGetTime(5));
+            PrintResult(Fibonacci.IterationGetTime(10));
+            PrintResult(Fibonacci.IterationGetTime(20));
+
+
+        }
+
+        static void PrintResult((int, TimeSpan) result)
+        {
+            var st = new StringBuilder();
+
+            st.AppendLine($"Член последовательности равный {result.Item1} найден за {result.Item2.TotalMicroseconds} микросекунд");
+
+            Console.WriteLine(st);
+        }
+
+        
+    }
+}
